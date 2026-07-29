a7b5fa9a2e8dd91df3fd68300f6efb6ee142cd4d
pmiyienda-dotcom <p.miyienda@alustudent.com>
2026-07-29 23:20:11 +0300

    debugging

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,10 @@
-# Increase worker_rlimit_nofile and worker_connections limit for Nginx
+# Optimizes Nginx file limits and max connections to handle extreme traffic load
 exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_processes 4;/worker_processes 4;\nworker_rlimit_nofile 65536;/g" /etc/nginx/nginx.conf;
-              /bin/sed -i "s/worker_connections 768;/worker_connections 4096;/g" /etc/nginx/nginx.conf;
+  command => '/bin/sed -i "s/^worker_connections.*/worker_connections 4096;/g" /etc/nginx/nginx.conf;
+              /bin/sed -i "/worker_processes/a worker_rlimit_nofile 65536;" /etc/nginx/nginx.conf;
+              /bin/echo "ULIMIT=\"-n 65536\"" >> /etc/default/nginx;
               /usr/sbin/service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+
 
