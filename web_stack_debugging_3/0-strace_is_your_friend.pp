34bf5147f511d83a6412652e037319c9ab8918a3
pmiyienda-dotcom <p.miyienda@alustudent.com>
2026-07-29 23:38:20 +0300

    debugging

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,9 @@
-# Optimizes Nginx file descriptors and worker settings to handle high concurrency
-exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_connections.*/worker_connections 4096;/g" /etc/nginx/nginx.conf;
-              /bin/sed -i "s/worker_processes.*/worker_processes 4;/g" /etc/nginx/nginx.conf;
-              /bin/echo "worker_rlimit_nofile 65536;" >> /etc/nginx/nginx.conf;
-              /bin/echo "ULIMIT=\"-n 65536\"" > /etc/default/nginx;
-              /usr/sbin/service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+# Puppet manifest to fix a typo in the WordPress core configuration causing Apache 500 error
+
+exec { 'fix-wordpress':
+  command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => '/bin/grep -q "class-wp-locale.phpp" /var/www/html/wp-settings.php',
 }
 
 
