4e97505f936ed2892a39dab38fd3071f6d917950
pmiyienda-dotcom <p.miyienda@alustudent.com>
2026-07-29 23:27:45 +0300

    debugging

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Configures Nginx to handle high-concurrency traffic loads without dropping requests
+exec { 'modify-nginx-limit':
+  command => '/bin/sed -i "s/15/2000/g" /etc/default/nginx; /usr/sbin/service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+
